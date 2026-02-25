2aa57cf6415e346d4fcab505cb622f2e010fcff0
fotohabilaj <fotohabilaj@gmail.com>
2026-02-25 12:48:24 +0100

    milestone 2 complete

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
-/*<div class="photocard">
-            <img id="pin" src="img/pin.svg" alt="">
-            <img id="pic" src="" alt="">
-            <p></p>
-        </div>
-*/
 
+const boardEl = document.querySelector('.photoboard')
+
+fetch('https://lanciweb.github.io/demo/api/pictures/')
+
+.then(Response => Response.json())
+.then(cards => {
+    console.log(cards);
+    
+    
+    for (let i = 0; i < cards.length; i++) {
+        const card = cards[i];
+        const {title,date,url}= card;
+        const markup = `<div class="photocard">
+            <img id="pin" src="img/pin.svg" alt="">
+            <img id="pic" src="${url}" alt="">
+            <p id="date">${date}</p>
+            <p id="title">${title}</p>
+        </div>`
+        boardEl.innerHTML += markup;
+    }
+    
+});
