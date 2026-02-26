7813e3d365b2994fab2b5e7eb6e5cca9ec61c8e5
fotohabilaj <fotohabilaj@gmail.com>
2026-02-26 16:17:39 +0100

    part 2 milestone 3 complete

--- js/main.js
+++ js/main.js
@@ -5,15 +5,13 @@ fetch('https://lanciweb.github.io/demo/api/pictures/')
 
 .then(Response => Response.json())
 .then(cards => {
-    console.log(cards);
-    
-    
+
     for (let i = 0; i < cards.length; i++) {
         const card = cards[i];
         const {title,date,url}= card;
         const markup = `<div class="photocard">
             <img id="pin" src="img/pin.svg" alt="">
-            <img onclick="overlay_on()" id="pic" src="${url}" alt="">
+            <img onclick="overlay_on(event)" id="pic" src="${url}" alt="">
             <p id="date">${date}</p>
             <p id="title">${title}</p>
         </div>`
@@ -21,10 +19,11 @@ fetch('https://lanciweb.github.io/demo/api/pictures/')
     }
     
 });
-
+const overImage = document.querySelector('.overimage')
 const overlayEl = document.querySelector('.overlay')
 
-function overlay_on (){
+function overlay_on (event){
+    overImage.src = event.target.src;
     overlayEl.style.display = "block";
 }
 
